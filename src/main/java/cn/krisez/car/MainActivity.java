9ffe2cd71873b4ba7ada76e98aa3db8c226a078d
Krisez <740879437@qq.com>
2018-07-19 16:50:41 +0800

    add lbs aMap

--- src/main/java/cn/krisez/car/MainActivity.java
+++ src/main/java/cn/krisez/car/MainActivity.java
@@ -1,6 +1,9 @@
 package cn.krisez.car;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +15,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptor;
+import com.amap.api.maps.model.BitmapDescriptorCreator;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.google.gson.JsonObject;
 
 import cn.krisez.car.ImageManage.ImgManager;
@@ -21,6 +38,11 @@ import cn.krisez.car.Network.NetUtil;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private MapView mvShow;
+    private MyLocationStyle myLocationStyle;
+    private AMap map ;
+    private BitmapDescriptor descriptor;
+    private boolean isLocate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +59,52 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        mvShow = findViewById(R.id.mv_show);
+        mvShow.onCreate(savedInstanceState);
+
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+
+        map = mvShow.getMap();
+
+        myLocationStyle.strokeColor(Color.parseColor("#ffffff"));
+        myLocationStyle.radiusFillColor(Color.argb(100,20,12,20));
+        map.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        map.setMyLocationEnabled(isLocate = true);
+        map.moveCamera(CameraUpdateFactory.zoomTo(17));
+        UiSettings settings = map.getUiSettings();
+        settings.setMyLocationButtonEnabled(true);
+
+        AMapLocationClient aMapLocationClient = new AMapLocationClient(this.getApplicationContext());
+        aMapLocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+                map.setMyLocationStyle(myLocationStyle);
+                isLocate = true;
+            }
+        });
+        AMapLocation location = aMapLocationClient.getLastKnownLocation();
+
+
+        map.setOnCameraChangeListener(new AMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                if(isLocate) {
+                    myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
+                    isLocate = false;
+                    map.setMyLocationStyle(myLocationStyle);
+                }
+            }
+
+            @Override
+            public void onCameraChangeFinish(CameraPosition cameraPosition) {
+
+            }
+        });
 
     }
+
 
     @Override
     public void onBackPressed() {
@@ -69,7 +135,9 @@ public class MainActivity extends AppCompatActivity
             startActivity(new Intent(MainActivity.this,PeopleActivity.class ));
             return true;
         }
-
+        descriptor = BitmapDescriptorFactory.fromAsset(item.getTitle()+".png");
+        myLocationStyle.myLocationIcon(descriptor);
+        map.setMyLocationStyle(myLocationStyle);
         return super.onOptionsItemSelected(item);
     }
 
@@ -96,5 +164,29 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mvShow.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mvShow.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mvShow.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mvShow.onDestroy();
     }
 }
