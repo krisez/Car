4cfaf1471b430328e039ffdaa155e5cdabf997f1
Krisez <740879437@qq.com>
2018-07-24 15:38:12 +0800

    ignore

---
+++ src/main/java/cn/krisez/car/entity/CarRoute.java
@@ -0,0 +1,34 @@
+package com.amap.map3d.demo.smooth;
+
+public class A {
+    /**
+     * systime : 2016-08-03 16:34:16
+     * lon : 106.499405
+     * loctime : 1470213257000
+     * address :
+     * speed : 0
+     * bearing : 221.36236572265625
+     * provider : gps
+     * accuracy : 12
+     * lat : 29.616063
+     */
+
+    private double lon;
+    private double lat;
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+}
